594bd3dd2520975e906ba8ca5c4a0030f91dde34
Nickolai <nipet18@student.sdu.dk>
2020-05-29 17:33:24 +0100

    New webpage + menubar + header

---
+++ app.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $(".hamburger").click(function () {
+        $(".wrapper").toggleClass("collapse");
+    });
+    
+    $("#home").click(function () {
+        $('.flex-container').load('Content.html');
+    });
+    
+    $("#profile").click(function () {
+        $('.flex-container').load('Profile.html');
+    });
+    
+    $("#courses").click(function () {
+        $('.flex-container').load('Courses.html');
+    });
+    
+    $("#timetable").click(function () {
+        $('.flex-container').load('Timetable.html');
+    });
+    
+    $("#mail").click(function () {
+        $('.flex-container').load('Mail.html');
+    });
+    
+    $("#selfservice").click(function () {
+        $('.flex-container').load('Selfservice.html');
+    });
+    
+    $("#lessontask").click(function () {
+        $('.flex-container').load('Lessontask.html');
+    });
+    
+    $("#booking").click(function () {
+        $('.flex-container').load('Booking.html');
+    });
+    
+    $("#library").click(function () {
+        $('.flex-container').load('Library.html');
+    });
+    
+    $(".seachbtn").click(function(){
+        $(this).toogleClass("bg-green");
+        $(".fas").toogleClass("color-white");
+    });
+});
+
+
