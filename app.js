621930fd40eee825e1a7e830f6096a8a4f9654df
Nickolai <nipet18@student.sdu.dk>
2020-06-09 13:29:11 +0100

    Content fix

--- app.js
+++ app.js
@@ -1,92 +1,95 @@
 $(document).ready(function () {
-    
+
     /*------loading the content on the mainpage-----*/
     $('.flex-container').load('Content.html');
-    
+
     /*----------------clickEvent side menu-------------*/
     $("#home").click(function () {
         $('.flex-container').load('Content.html');
     });
-    
+
     $("#profile").click(function () {
         $('.flex-container').load('Profile.html');
     });
-    
+
     $("#courses").click(function () {
         $('.flex-container').load('Courses.html');
     });
-    
+
     $("#timetable").click(function () {
         $('.flex-container').load('Timetable.html');
     });
-    
+
     $("#mail").click(function () {
         $('.flex-container').load('Mail.html');
     });
-    
+
     $("#selfservice").click(function () {
         $('.flex-container').load('Selfservice.html');
     });
-    
+
     $("#lessontask").click(function () {
         $('.flex-container').load('Lessontask.html');
     });
-    
+
     $("#booking").click(function () {
         $('.flex-container').load('Booking.html');
     });
-    
+
     $("#library").click(function () {
         $('.flex-container').load('Library.html');
     });
-    
+
     $("#Print").click(function () {
         $('.flex-container').load('Print.html');
     });
-    
+
     $("#Software").click(function () {
         $('.flex-container').load('Software.html');
     });
-    
+
     /*--------------clickEvent "Courses"-----------*/
-    
-    $( "courses" ).on( "click", "coursesDesign",function () {
+
+    $("courses").on("click", "coursesDesign", function () {
         $('.flex-container').load('CoursesDesign.html');
     });
-    
+
     $("#CoursesDesign").click(function () {
         $('index.flex-container').load('CoursesDesign.html');
     });
-    
+
     $("#coursesDP").click(function () {
         $('.flex-container').load('CoursesDP.html');
     });
-    
+
     $("#courses3D").click(function () {
         $('.flex-container').load('Courses3D.html');
     });
-    
+
     /*--------------side menu collapse-----------*/
-    
+
     $(".hamburger").click(function () {
         $(".wrapper").toggleClass("collapse");
     });
-    
+
     /*------search animation------*/
-    
-    $(".searchbtn").click(function(){
+
+    $(".searchbtn").click(function () {
         $(this).toggleClass("bg-blue");
         $(".fas").toggleClass("color-white");
         $(".searchInput").toggleClass("active-width");
     });
-    
+
     /*-----Dark/white toggle-----------*/
-    
-    $(function() {
-    $(".switch").click(function() {
-        $("body").toggleClass("dark-mode");
+
+
+        $(".switch").click(function () {
+            $("body").toggleClass("dark-mode");
         });
-    });
+
+    /*---------content draggable--------*/
+    
+
+        
+
 });
-
-
